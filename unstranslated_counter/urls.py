59817660cc262ea414c35d8cd40396ee91cb75e9
sadeq-asad <ammanflowers77@yahoo.com>
2018-07-19 00:23:37 +0300

    Media_Observer

--- unstranslated_counter/urls.py
+++ unstranslated_counter/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
+#from django.urls import path
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    path('', views.overview, name='overview'),
+    url('', views.overview, name='overview'),
     ]
