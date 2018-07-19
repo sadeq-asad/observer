bb6b9cc610e2e83cd0c9916b3dbed50b74f4052b
sadeq-asad <40718146+sadeq-asad@users.noreply.github.com>
2018-07-19 17:13:57 +0300

    Update views.py

--- unstranslated_counter/views.py
+++ unstranslated_counter/views.py
@@ -3,10 +3,8 @@ from . import requests
 
 # Create your views here.
 login_page = 'http://office.mediaobserver-me.com:882/checkuser.php'
-# client = orange (customers=173)
 credentials = {'username':'*****','password':'*****'}
-accounts = {'LG':166, 'Orange':173}
-#'ACT':167, 'Emaar':177, 'AIG4':288, 'Crescent':307, 'Pearl':334, 'HBKU':366, 'Total':624, 'Westin':658, 'Bein':856, 'Fine':910, 'Alan':1050, 'DB':1052, 'DCCI-tr':1132, 'AIG_clips':1352, 'Danube':1750, 'USAID':1826, 'Memac':2060, 'Disney':211}
+accounts = {'LG':166, 'Orange':173,'ACT':167, 'Emaar':177, 'AIG4':288, 'Crescent':307, 'Pearl':334, 'HBKU':366, 'Total':624, 'Westin':658, 'Bein':856, 'Fine':910, 'Alan':1050, 'DB':1052, 'DCCI-tr':1132, 'AIG_clips':1352, 'Danube':1750, 'USAID':1826, 'Memac':2060, 'Disney':211}
 
 def overview(request):  
     # create session, enter credentials, get and render cookies
@@ -25,13 +23,7 @@ def overview(request):
         # contents
         content = str(arts_list.content)
         text = content.split(',')
-        
-        # search for untranslated items, update counter flag
-        #if account_name in ['Crescent', 'Pearl', 'Total', 'Westin', 'Bein', 'DB', 'DCCI-tr', 'AIG_clips', 'Danube', 'Memac', 'Disney']:
-            #for word in text:
-                #if word in ['"headline_translated":""', '"headline_translated":null', '"text_translated":""'] and word != '"check_body":null':
-                    #untranslated_arts += 1            
-        #else:
+    
         if '"check_body":null' in text: #check if headline
             for word in text:
                 if word in ['"headline_translated":""', '"headline_translated":null']:
